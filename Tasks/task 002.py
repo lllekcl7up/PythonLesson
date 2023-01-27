7adb7b33bab17bb811dd82dac17bf3b34c7c84ef
Skorobogatov Dima <lllekcl7up@gmail.com>
2023-01-27 19:54:29 +0300

    Добавил решение 2ой задачи

--- Tasks/task 002.py
+++ Tasks/task 002.py
@@ -1,16 +1,11 @@
 # Задача 2: Найдите сумму цифр трехзначного числа. 
 
-number = int(input("Введите трёх значное число: "))
+number = int(input("Введите число: "))
 sum = 0
-if number < 0:
-    number = number * - 1
+if number < 0: # Проверяем на отрицательность 
+    number = number * - 1 # если да , то делаем положительным
 while (number > 0):
         num = number % 10
         sum = sum + num
         number = number // 10
 print("Сумма цифр равна: ", sum)
-
-
-
-
-
