c25612530553f037b65562e51dd04a6f578790da
Dima Skorobogatov <lllekcl7up@gmail.com>
2023-02-03 17:39:47 +0300

    Задача 14

--- Tasks/task 014.py
+++ Tasks/task 014.py
@@ -2,3 +2,15 @@
 # вида 2k), не превосходящие числа N.
 
 # 10 -> 1 2 4 8
+
+number = int(input('Введите число: '))
+
+def DegreeTwo(n):
+    degree = []
+    for i in range(n):
+        k = 2**i
+        if k <= n:
+            degree.append(k)
+    return degree
+
+print(DegreeTwo(number))
