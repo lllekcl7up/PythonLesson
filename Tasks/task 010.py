618edefee1622631a8e21069accc1206237ea366
Dima Skorobogatov <lllekcl7up@gmail.com>
2023-02-03 09:06:14 +0300

    Задача 10

---
+++ Tasks/task 010.py
@@ -0,0 +1,31 @@
+# Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх
+# решкой, а некоторые – гербом. Определите минимальное число
+# монеток, которые нужно перевернуть, чтобы все монетки были
+# повернуты вверх одной и той же стороной. Выведите минимальное
+# количество монет, которые нужно перевернуть.
+# 5 -> 1 0 1 1 0
+# 2
+
+from random import randint as rndm
+
+number = int(input("Введите число монеток: "))
+coins = []
+for i in range(number):
+    coins.append(rndm(0,1))
+print(coins)
+
+def HeadsAndTails(list1):
+    tails = 0
+    heads = 0
+    for i in range(len(list1)):
+        if list1[i] == 1:
+            heads +=1
+        else:
+            tails +=1
+    if heads < tails:
+        return heads
+    else:
+        return tails
+
+print(HeadsAndTails(coins))
+
