3960b6a29da8f36bda8d84617707220bae469554
Skorobogatov Dima <lllekcl7up@gmail.com>
2023-02-23 11:25:07 +0300

    Phonebook

--- Tasks/Phonebook/operations.py
+++ Tasks/Phonebook/operations.py
@@ -1,15 +1,15 @@
 def print_book():
-    phonebook = open('/home/dima/MyWork/PythonLesson/Tasks/Phonebook/phone_book.txt','r',encoding='utf-8')
+    phonebook = open('S:\WorkGit\PythonLesson\Tasks\Phonebook\phone_book.txt','r',encoding='utf-8')
     print(phonebook.read())
     phonebook.close()
 
 def add_record(data):
-    phonebook = open('/home/dima/MyWork/PythonLesson/Tasks/Phonebook/phone_book.txt','a',encoding='utf-8')
+    phonebook = open('S:\WorkGit\PythonLesson\Tasks\Phonebook\phone_book.txt','a',encoding='utf-8')
     phonebook.write(' '.join(data)+'\n')
     phonebook.close()
 
 def find_record(name):
-    phonebook = open('/home/dima/MyWork/PythonLesson/Tasks/Phonebook/phone_book.txt','r', encoding='utf-8')
+    phonebook = open('S:\WorkGit\PythonLesson\Tasks\Phonebook\phone_book.txt','r', encoding='utf-8')
     list_of_found_record = list()
     for lines in phonebook:
         # print(lines)
@@ -19,23 +19,20 @@ def find_record(name):
     return list_of_found_record
 
 def change_record(meaning,new_meaning):
-    with open(r'/home/dima/MyWork/PythonLesson/Tasks/Phonebook/phone_book.txt', 'r',encoding='utf-8') as phonebook:
+    with open('S:\WorkGit\PythonLesson\Tasks\Phonebook\phone_book.txt', 'r',encoding='utf-8') as phonebook:
         book = phonebook.read()
-    with open(r'/home/dima/MyWork/PythonLesson/Tasks/Phonebook/phone_book.txt', 'w',encoding='utf-8') as phonebook:
+    with open('S:\WorkGit\PythonLesson\Tasks\Phonebook\phone_book.txt', 'w',encoding='utf-8') as phonebook:
         book = book.replace(meaning,new_meaning)
         phonebook.write(book)
     return book
 
 
 def delete_record(name):
-    with open('/home/dima/MyWork/PythonLesson/Tasks/Phonebook/phone_book.txt', 'r') as phonebook:
+    with open('S:\WorkGit\PythonLesson\Tasks\Phonebook\phone_book.txt', 'r',encoding='utf-8') as phonebook:
         book = phonebook.readlines()
         phonebook.seek(0)
-    with open('/home/dima/MyWork/PythonLesson/Tasks/Phonebook/phone_book.txt', 'w') as phonebook:
+    with open('S:\WorkGit\PythonLesson\Tasks\Phonebook\phone_book.txt', 'w',encoding='utf-8') as phonebook:
         for line in book:
             if name not in line: 
                 phonebook.write(line)
     print("Удалено")
-
-
-
